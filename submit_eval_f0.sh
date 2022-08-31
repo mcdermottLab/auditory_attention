ce8f1cecf811fadb883149b14a2d5c291e8678de
imgriff <imgriffith@berkeley.edu>
2022-08-31 09:48:54 -0400

    use pitch conda env for f0 test

--- submit_eval_f0.sh
+++ submit_eval_f0.sh
@@ -13,7 +13,7 @@ module add openmind/miniconda
 module add openmind/cudnn/11.5-v8.3.3.40 
 module add openmind/cuda/11.3
 
-source activate /om2/user/imgriff/conda_envs/torch_11_cuda_11
+source activate /om2/user/imgriff/conda_envs/torch_11_cuda_11_pitch
 
 python3 eval_full_snr_range.py --gpus 1 --n_jobs 20 --exp_dir "attn_cue_models/attn_f0_test" \
                                --array_id $SLURM_ARRAY_TASK_ID \
