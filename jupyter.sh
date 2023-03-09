9dbceb64b1689a2595716ac88e293d0afacaca57
Ian <iangriffith@g.harvard.edu>
2023-03-08 20:12:00 -0500

    Commonvoice Format

--- jupyter.sh
+++ jupyter.sh
@@ -2,8 +2,8 @@
 #SBATCH --job-name=jupyter_notebook
 #SBATCH --output=outLogs/notebook%j.out
 #SBATCH --error=outLogs/notebook%j.err
-#SBATCH --mem=24Gb
-#SBATCH -n 20 
+#SBATCH --mem=12Gb
+#SBATCH -n 1 
 #SBATCH -N 1
 #SBATCH --time=6:00:00
 #SBATCH --partition=mcdermott
