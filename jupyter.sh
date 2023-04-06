018f3a9eedc116a1656d9172f4de5798e1991446
Robert P Hess <rphess@openmind7.ib.cluster>
2023-04-06 13:31:19 -0400

    add h5

--- jupyter.sh
+++ jupyter.sh
@@ -2,22 +2,18 @@
 #SBATCH --job-name=jupyter_notebook
 #SBATCH --output=outLogs/notebook%j.out
 #SBATCH --error=outLogs/notebook%j.err
-#SBATCH --mem=6Gb
-#SBATCH -n 1 
+#SBATCH --mem=6Gb 
 #SBATCH --time=6:00:00
-#SBATCH --partition=mcdermott
+#SBATCH --partition=normal
 #SBATCH --cpus-per-task=1
 #SBATCH -x node[100-115]
 module add openmind/miniconda
 
 
-
-export CONDA_ENVS_PATH=~/my-envs:/om2/user/imgriff/conda_envs
-
-source activate /om2/user/imgriff/conda_envs/aligner
+source activate /om2/user/imgriff/conda_envs/torch_11_cuda_11_pitch
 
 
 
 
-export LC_ALL=C; unset XDG_RUNTIME_DIR && jupyter notebook --no-browser --ip='0.0.0.0' --port=1337
+export LC_ALL=C; unset XDG_RUNTIME_DIR && jupyter notebook --no-browser --ip='0.0.0.0' --port=1492
 
