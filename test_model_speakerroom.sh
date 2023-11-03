67e5d74ac11f3be54c26e50210f4e6a8168a041b
Ian <iangriffith@g.harvard.edu>
2023-11-03 19:10:13 -0400

    Using most recent binaural ckpt

--- test_model_speakerroom.sh
+++ test_model_speakerroom.sh
@@ -19,8 +19,8 @@ module add openmind/cudnn/11.5-v8.3.3.40
 module add openmind/cuda/11.3
 
 
-python3 eval_binaural.py --config config/binaural_attn/word_task_mixed_cue_large_architecture_v03.yml \
-                 --ckpt_path attn_cue_models/word_task_mixed_cue_large_architecture_v04/checkpoints/epoch=0-step=1000-v8.ckpt \
+python3 eval_binaural.py --config config/binaural_attn/word_task_mixed_cue_large_architecture_v04.yml \
+                 --ckpt_path attn_cue_models/word_task_mixed_cue_large_architecture_v04/checkpoints/epoch=0-step=2000-v14.ckpt \
                  --model_name word_task_mixed_cue_large_architecture_v04 --location_idx $SLURM_ARRAY_TASK_ID \
                  --gpus 1 --n_jobs 1 --exp_dir binaural_eval/ \
                  --snr 0 --cue_type voice_and_location
