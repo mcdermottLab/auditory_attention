db12dbbf364b07c444c952d9218b59951150c5f0
Ian <iangriffith@g.harvard.edu>
2023-11-16 17:02:52 -0500

    Run mono eval for longer

--- submit_scripts/eval_new_mono_on_timit.sh
+++ submit_scripts/eval_new_mono_on_timit.sh
@@ -4,7 +4,7 @@
 #SBATCH --error=outLogs/eval_concat_timit_%j.err
 #SBATCH --mem=60Gb
 #SBATCH --cpus-per-task=10
-#SBATCH --time=3:00:00
+#SBATCH --time=4:00:00
 #SBATCH --partition=mcdermott
 #SBATCH --gres=gpu:1
 
@@ -17,4 +17,4 @@ source activate /om2/user/imgriff/conda_envs/torch_11_cuda_11_pitch
 python3 eval_timit.py --gpus 1 --n_jobs 10 --exp_dir "attn_cue_models/attn_timit_task" \
                       --model_name "word_task_mono_arch_v04" \
                       --config_name "config/binaural_attn/word_task_mono_arch_v04.yaml" \
-                      --ckpt_path "attn_cue_models/word_task_mono_arch_v04/checkpoints/epoch=1-step=3893-v5.ckpt"
+                      --ckpt_path "attn_cue_models/word_task_mono_arch_v04/checkpoints/epoch=1-step=3893-v8.ckpt"
