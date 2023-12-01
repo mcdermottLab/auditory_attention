3f6554437308e5e986c1a7e96e6e209d8e14db58
Ian <iangriffith@g.harvard.edu>
2023-12-01 11:02:56 -0500

    Dev tfrecords reading for dual-task pre-training

--- jupyter_torch_tf.sh
+++ jupyter_torch_tf.sh
@@ -2,10 +2,10 @@
 #SBATCH --job-name=jupyter_notebook
 #SBATCH --output=outLogs/notebook_%j.out
 #SBATCH --error=outLogs/notebook_%j.err
-#SBATCH --mem=20Gb 
-#SBATCH --time=6:00:00
+#SBATCH --mem=8Gb 
+#SBATCH --time=1:00:00
 #SBATCH --partition=mcdermott
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=5
 #SBATCH -x node104
 
 
@@ -17,7 +17,7 @@ module add openmind/miniconda
 export HDF5_USE_FILE_LOCKING=FALSE
 
 
-source activate /om2/user/imgriff/conda_envs/torch_11_cuda_11_pitch
+source activate /om/user/imgriff/conda_envs/torch_2_tf
 
 
 export LC_ALL=C; unset XDG_RUNTIME_DIR && jupyter notebook --no-browser --ip='0.0.0.0' --port=1337 --NotebookApp.allow_origin='*'
