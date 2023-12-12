10c17bd7c245b7d2a9bfe1fb25e4d39a1463a523
Ian <iangriffith@g.harvard.edu>
2023-12-12 08:33:16 -0500

    update control model plots

--- jupyter.sh
+++ jupyter.sh
@@ -18,4 +18,4 @@ source activate /om2/user/imgriff/conda_envs/torch_11_cuda_11_pitch
 
 
 
-export LC_ALL=C; unset XDG_RUNTIME_DIR && jupyter notebook --no-browser --ip='0.0.0.0' --port=1492
+export LC_ALL=C; unset XDG_RUNTIME_DIR && jupyter notebook --no-browser --ip='0.0.0.0' --port=1337
