f975ad16639b5d2dcdcffdd31de0e1b300ec9739
Ian <iangriffith@g.harvard.edu>
2023-12-14 00:23:23 -0500

    Plot voice + loc v04 model

--- train_binaural_single_model_all_coloc.sh
+++ train_binaural_single_model_all_coloc.sh
@@ -2,9 +2,9 @@
 #SBATCH --job-name=train_binaural_attn
 #SBATCH --output=outLogs/train_binaural_attn_%j.out
 #SBATCH --error=outLogs/train_binaural_attn_%j.err
-#SBATCH --mem=400Gb
+#SBATCH --mem=100Gb
 #SBATCH -N 1
-#SBATCH --cpus-per-task=48
+#SBATCH --cpus-per-task=16
 #SBATCH --time=3-00:00:00
 #SBATCH --partition=mcdermott
 #SBATCH --gres=gpu:a100:4
@@ -22,7 +22,7 @@ source activate /om2/user/imgriff/conda_envs/pytorch_2
 #module add openmind/cuda/12.3
 
 which python3
-python3 spatialtrain.py --config config/binaural_attn/word_task_sans_loc_only_cue_v04.yml \
-                 --gpus 4 --n_jobs 12 --resume_training True \
+python3 spatialtrain.py --config config/binaural_attn/word_task_all_co_located_v04.yml \
+                 --gpus 4 --n_jobs 4 --resume_training True \
                  --exp_dir attn_cue_models \
 
