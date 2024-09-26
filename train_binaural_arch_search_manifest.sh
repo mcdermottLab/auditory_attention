c017d6f84d91d011b0b203f39119b102b346542e
Robert P Hess <rphess@ranvier.ib.cluster>
2024-09-26 14:04:39 -0400

    Add batch script

---
+++ train_binaural_arch_search_manifest.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH --job-name=train_binaural_attn
+#SBATCH --output=outLogs/train_binaural_arch_search_v09_%A_%a.out
+#SBATCH --error=outLogs/train_binaural_arch_search_v09_%A_%a.err
+#SBATCH --mem=100GB
+#SBATCH -N 1
+#SBATCH --cpus-per-task=16
+#SBATCH --time=2-00:00:00
+#SBATCH --partition=normal
+#SBATCH --gres=gpu:a100:4
+#SBATCH --array=0-9
+
+#source /etc/profile.d/modules.sh
+#module use /cm/shared/modulefiles
+
+source /etc/profile.d/modules.sh
+module load openmind8/anaconda/3-2022.10
+
+export HDF5_USE_FILE_LOCKING=FALSE
+
+source activate /om2/user/imgriff/conda_envs/pytorch_2
+
+#module add openmind/cudnn/11.5-v8.3.3.40
+#module add openmind/cuda/12.3
+
+which python3
+python3 spatialtrain.py --config_list /om2/user/rphess/Auditory-Attention/binaural_train_manifests/arch_search_main.pkl --job_id $SLURM_ARRAY_TASK_ID \
+                 --gpus 4 --n_jobs 4  --resume_training True \
+                 --exp_dir /om2/user/rphess/Auditory-Attention/attn_cue_models \
