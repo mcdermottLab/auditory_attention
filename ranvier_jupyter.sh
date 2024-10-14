f78052b7602e8425b437322b00f4f1e4b9198c3d
imgriff <imgriff@mit.edu>
2024-10-14 18:25:21 -0400

    Keep last changes

---
+++ ranvier_jupyter.sh
@@ -0,0 +1,35 @@
+er=$(whoami)
+node=$(hostname -s)
+port=1338
+
+
+module add openmind/miniconda
+source activate /om2/user/imgriff/conda_envs/torch_11_cuda_11_pitch
+
+
+
+if [ "$1" != "" ]; then
+    echo "USING SPECIFIED PORT: $1"
+    port=$1
+fi
+
+if [ $HOSTNAME == ranvier ] &&  [ -z "$SLURM_CLUSTER_NAME" ]; then
+    cluster="ranvier.mit.edu"
+else
+    cluster="openmind7.mit.edu"
+fi
+
+if [ "$user" != "msaddler" ] && [ "$port" == "8998" ]; then
+    echo "PORT 8998 IS RESERVED FOR msaddler; PLEASE SPECIFY ANOTHER PORT"
+else
+    # Print instructions to create ssh tunnel
+    echo -e "
+    >>> Command to create ssh tunnel: <<<
+    ssh -N -f -L ${port}:${node}:${port} ${user}@${cluster}
+    >>> Address for jupyter-notebook: <<<
+    localhost:${port}
+    "
+
+    unset XDG_RUNTIME_DIR
+    jupyter notebook --no-browser --port=${port} --ip="0.0.0.0"
+fi
--- train_binaural_v09_control_manifest.sh
+++ train_binaural_v09_control_manifest.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --job-name=train_binaural_attn
-#SBATCH --output=outLogs/train_binaural_attn_v09_%A_%a.out
-#SBATCH --error=outLogs/train_binaural_attn_v09_%A_%a.err
+#SBATCH --output=outLogs/train_binaural_attn_v09_control_%A_%a.out
+#SBATCH --error=outLogs/train_binaural_attn_v09_control_%A_%a.err
 #SBATCH --mem=100GB
 #SBATCH -N 1
 #SBATCH --cpus-per-task=16
-#SBATCH --time=4-00:00:00
-#SBATCH --partition=mcdermott
+#SBATCH --time=2-00:00:00
+#SBATCH --partition=normal
 #SBATCH --gres=gpu:a100:4
 #SBATCH --array=0-3 # 0-3; 3 models in manifest
 
@@ -26,6 +26,5 @@ source activate /om2/user/imgriff/conda_envs/pytorch_2
 
 which python3
 python3 spatialtrain.py --config_list /om2/user/imgriff/projects/torch_2_aud_attn/binaural_train_manifests/v09_control_arch_manifest.pkl --job_id $SLURM_ARRAY_TASK_ID \
-                 --gpus 4 --n_jobs 4  \
+                 --gpus 4 --n_jobs 16  --resume_training  True \
                  --exp_dir /om2/user/imgriff/projects/torch_2_aud_attn/attn_cue_models \
-                #  --resume_training True \ 
