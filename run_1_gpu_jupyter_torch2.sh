4904b51d65bb341b307338480487b42cd94d72db
imgriff <imgriff@mit.edu>
2025-04-03 16:55:59 -0400

    modify train for quick train

--- run_1_gpu_jupyter_torch2.sh
+++ run_1_gpu_jupyter_torch2.sh
@@ -5,17 +5,14 @@
 #SBATCH --mem=16Gb
 #SBATCH --cpus-per-task=2
 #SBATCH --time=3:00:00
-#SBATCH --partition=mcdermott
-#SBATCH --gres=gpu:a100:1 
-#SBATCH -x dgx001,dgx002,node093
+#SBATCH --partition=ou_bcs_high
+#SBATCH --gres=gpu:h100:1
 
-# module load /openmind/miniconda
-module load openmind8/anaconda/3-2022.10
-
+module load miniforge
 
 export HDF5_USE_FILE_LOCKING=FALSE
 
-source activate /om2/user/imgriff/conda_envs/pytorch_2
+mamba activate pytorch_2
 
 export LC_ALL=C; unset XDG_RUNTIME_DIR && jupyter notebook --no-browser --ip='0.0.0.0' --port=1338
 
