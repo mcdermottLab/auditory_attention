54a9669f941d095a20df9130df9029848871cfa4
Ian <iangriffith@g.harvard.edu>
2025-06-01 17:06:12 -0400

    Train backbone on saddler task

--- train_binaural_backbone.sh
+++ train_binaural_backbone.sh
@@ -2,10 +2,10 @@
 #SBATCH --job-name=train_backbone
 #SBATCH --output=outLogs/train_word_task_v10_backbone_word_babble_and_noise_%j.out
 #SBATCH --error=outLogs/train_word_task_v10_backbone_word_babble_and_noise_%j.err 
-#SBATCH --mem=500Gb
+#SBATCH --mem=200Gb
 #SBATCH --cpus-per-task=64
-#SBATCH --partition=mcdermott
-#SBATCH --time=4-00:00:00
+#SBATCH --partition=normal
+#SBATCH --time=2-00:00:00
 #SBATCH --gres=gpu:a100:4
 
 #source /etc/profile.d/modules.sh
@@ -16,7 +16,7 @@ module load openmind8/anaconda/3-2022.10
 
 export HDF5_USE_FILE_LOCKING=FALSE
 
-source activate /om2/user/imgriff/conda_envs/pytorch_2
+source activate /om/user/imgriff/conda_envs/pytorch_2_tf
 #module add openmind/cudnn/11.5-v8.3.3.40
 #module add openmind/cuda/12.3
 
