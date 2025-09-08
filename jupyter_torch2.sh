ef8db6586b79d2322ab137166da47ad1d055cb4b
imgriff <imgriff@mit.edu>
2025-09-08 00:29:56 -0400

    fig 6 add separated conf and accuracy for model-model rebuttal

--- jupyter_torch2.sh
+++ jupyter_torch2.sh
@@ -4,16 +4,18 @@
 #SBATCH --error=outLogs/notebook_%j.err
 #SBATCH --mem=50Gb 
 #SBATCH --time=3:00:00
-#SBATCH --partition=mcdermott
+#SBATCH --partition=ou_bcs_high
 #SBATCH --cpus-per-task=20
-#SBATCH -x dgx001,dgx002
 
-source /etc/profile.d/modules.sh
-module add openmind/miniconda
+module add miniforge
 
 export HDF5_USE_FILE_LOCKING=FALSE
 
-source activate /om2/user/imgriff/conda_envs/pytorch_2_sva
+mamba activate pytorch_2
+#module add openmind/cudnn/11.5-v8.3.3.40
+#module add openmind/cuda/12.3
+
+which python3
 
 
 export LC_ALL=C; unset XDG_RUNTIME_DIR && jupyter notebook --no-browser --ip='0.0.0.0' --port=1337
