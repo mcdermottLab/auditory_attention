3d035557d651a6410356dc62b9cb3223f5982a4d
imgriff <imgriff@mit.edu>
2025-09-15 11:26:48 -0400

    engaging noteboks

--- corpus/binaural_attention_h5.py
+++ corpus/binaural_attention_h5.py
@@ -20,6 +20,7 @@ class BinauralAttentionDataset(torch.utils.data.ConcatDataset):
                  gender_balanced_4M=False,
                  mostly_same_dist=False,
                  cue_free_percentage=None,
+                 return_azim_loc_only=False,
                  mixture_percentages={'voice_and_location':.33, 'voice_only':.33, "location_only":.33}, **kwargs):
         """
         Builds the pytorch hdf5 combined dataset from the files found in the
@@ -93,13 +94,10 @@ class BinauralAttentionDataset(torch.utils.data.ConcatDataset):
             print(f"{mixture_percentages=}")
 
         print(f"{len(self.all_hdf5_files)} files in {mode} concat dataset")
-        if self.v05:
-            dataset_class = H5DatasetV05
-        elif self.v06:
-            dataset_class = H5DatasetV06
-        else:
-            dataset_class = H5Dataset
-        self.all_hdf5_datasets = [dataset_class(h5_file, cue_type, task, batch_size, run_mono, skip_negative_elev, mono_sanity_check, clean_percentage, mixture_percentages, no_cue_on_clean) for h5_file in self.all_hdf5_files]
+
+        dataset_class = H5DatasetV06
+
+        self.all_hdf5_datasets = [dataset_class(h5_file, cue_type, task, batch_size, run_mono, skip_negative_elev, mono_sanity_check, clean_percentage, mixture_percentages, no_cue_on_clean, return_azim_loc_only) for h5_file in self.all_hdf5_files]
 
         super().__init__(self.all_hdf5_datasets)
 
@@ -111,279 +109,9 @@ class BinauralAttentionDataset(torch.utils.data.ConcatDataset):
         return class_map
 
 
-class H5Dataset(torch.utils.data.Dataset):
-    def __init__(self, path, cue_type, task, batch_size, run_mono, skip_negative_elev=False, mono_sanity_check=False, 
-                clean_percentage=0.0, mixture_percentages={'voice_and_location':.33, 'voice_only':.33, "location_only":.33}, no_cue_on_clean=False):
-        """
-        Builds a pytorch hdf5 dataset
-        Args:
-            path (str): location of the hdf5 dataset
-            task (str): string indicating label keys to return 
-        """
-        self.file_path = Path(path).as_posix()
-        self.dataset = None
-        self.task = task
-        self.run_mono = run_mono
-        self.batch_size = batch_size
-        self.skip_negative_elev = skip_negative_elev
-        self.mono_sanity_check = mono_sanity_check
-        self.clean_percentage = clean_percentage
-        self.no_cue_on_clean = no_cue_on_clean
-        self.batch_ixs = np.arange(self.batch_size)
-        if self.skip_negative_elev:
-            print("Skipping negative elevations")
-        else:
-            print("Including negative elevations")
-
-        if "v02" in self.file_path:
-            self.voice_key = "voice_cue_rand_loc"
-        elif "v03" in self.file_path:
-            self.voice_key = "voice_cue_center_loc"
-        else:
-            self.voice_key = "voice_cue_target_loc"
-
-        if cue_type == 'voice_and_location':
-            self.cue_key = 'voice_cue_target_loc'
-        elif cue_type == 'voice':
-            self.cue_key = self.voice_key
-        elif cue_type == "location":
-            self.cue_key = "loc_cue"
-        elif cue_type == "mixed":
-            self.cue_key = "mixed_cue"
-            self.voice_and_loc_size = int(batch_size * mixture_percentages['voice_and_location'])
-            self.voice_only_percent_size = int(batch_size * mixture_percentages['voice_only'])
-            self.location_only_percent_size = int(batch_size * mixture_percentages['location_only']) 
-
-        if self.task == 'word':
-            self.label_key = 'word_int'
-        elif self.task == 'location':
-            self.label_key = ('label_loc_target_azim', 'label_loc_target_elev')
-        elif self.task == 'word_and_location':
-            self.label_key = ('word_int', 'label_loc_target_azim', 'label_loc_target_elev')
-
-        with h5py.File(self.file_path, 'r', swmr=True) as file:
-            self.dataset_len = len(file['target']) // self.batch_size
-
-    def azim_elev_to_label(self, azim, elev):
-        if self.skip_negative_elev:
-            return np.array(((elev / 10) * 72) + (azim / 5) + 1, dtype=np.int64)
-        else:
-            # + 40 is so lowest elevation is 0 in label index
-            return np.array((((elev + 40) / 10) * 72) + (azim / 5), dtype=np.int64)
-
-    def __getitem__(self, index):
-        """
-        Returns examples from the hdf5 file.
-        Args: 
-            index (int): index into the hdf5 file
-        Returns:
-            cue (np.array): the cue audio 
-            foreground (np.array): the target audio 
-            background (np.array): the distractor audio 
-            label (np.array): the label for the batch
-        """
-        start = index * self.batch_size
-        end = start + self.batch_size
-        if self.dataset is None:
-            self.dataset = h5py.File(self.file_path, 'r', swmr=True)
-        # Updates on 10/11/2023 - use "both_cue" for 2/3 of mixed cue training
-        # Will use different backgrounds for voice-only condition cue type
-        if self.cue_key == 'mixed_cue':
-            # get cut sizes for each condition
-            loc_only_ix = start + self.location_only_percent_size
-            normal_ix = loc_only_ix + self.voice_and_loc_size
-            co_located_ix = normal_ix + self.voice_only_percent_size
-            # cue handles 
-            loc_cue = self.dataset['loc_cue']
-            # use both cue for voice + location and voice only conditions
-            both_cue = self.dataset['voice_cue_target_loc']
-            cues1 = loc_cue[start : loc_only_ix].transpose((0, 2, 1))
-            cues2 = both_cue[loc_only_ix : normal_ix].transpose((0, 2, 1)) 
-            cues3 = both_cue[normal_ix : end].transpose((0, 2, 1)) 
-            # first 1/3 location, last 2/3 both
-            cue = np.concatenate((cues1, cues2, cues3), axis=0)
-            ## get backgrounds for mixed cue condition
-            # get backgrounds for location only and voice + location conditions
-            bg_1 = self.dataset['bg_scene'][start:loc_only_ix].transpose((0, 2, 1))
-            bg_2 = self.dataset['bg_scene'][loc_only_ix:normal_ix].transpose((0, 2, 1))
-            bg_3 = self.dataset['bg_scene_co_located'][normal_ix: end].transpose((0, 2, 1))
-            # first 2/3 normal, last 1/3 co-located
-            background = np.concatenate((bg_1, bg_2, bg_3), axis=0) 
-
-        elif self.cue_key == 'voice':
-            cue = self.dataset[self.cue_key][start:end].transpose((0, 2, 1))
-            background = self.dataset['bg_scene_co_located'][start:end].transpose((0, 2, 1))
-        else:
-            cue = self.dataset[self.cue_key][start:end].transpose((0, 2, 1))
-            background = self.dataset['bg_scene'][start:end].transpose((0, 2, 1))
-
-        if np.isnan(cue).all():
-            cue[:] = 0
-        foreground = self.dataset['target'][start:end].transpose((0, 2, 1))
-
-        if self.clean_percentage > 0.0:
-            num_clean = int(self.clean_percentage * self.batch_size)
-            clean_idx = np.random.choice(self.batch_ixs, num_clean, replace=False)
-            background[clean_idx, :, :] = 0
-            if self.no_cue_on_clean:
-                cue[clean_idx, :, :] = 0
-
-        # if self.skip_negative_elev and self.dataset['label_loc_target_elev'][start:end] < 0:
-        #     return None, None, None, None
-
-        if self.task == 'word':
-            label = self.dataset[self.label_key][start:end]
-        elif self.task == 'location':
-            azim = self.dataset[self.label_key[0]][start:end]
-            elev = self.dataset[self.label_key[1]][start:end]
-            label = []
-            for azim, elev in zip(azim, elev):
-                label.append(self.azim_elev_to_label(azim, elev))
-        else:
-            word = self.dataset[self.label_key[0]][start:end]
-            azim = self.dataset[self.label_key[1]][start:end]
-            elev = self.dataset[self.label_key[2]][start:end]
-            loc = self.azim_elev_to_label(azim, elev)
-            label = np.stack((word, loc), axis=1)
-
-
-        if self.run_mono:
-            # average l & r channels
-            cue = cue.mean(1).reshape(self.batch_size, -1)
-            foreground = foreground.mean(1).reshape(self.batch_size, -1)
-            background = background.mean(1).reshape(self.batch_size, -1)
-            
-        if self.mono_sanity_check:
-            # use only left channel for both channels
-            cue[:,1,:] = cue[:,0,:]
-            foreground[:,1,:] = foreground[:,0,:]
-            background[:,1,:] = background[:,0,:]
-            
-        return cue, foreground, background, label
-
-    def __len__(self):
-        return self.dataset_len
-    
-
-
-class H5DatasetV05(torch.utils.data.Dataset):
-    def __init__(self, path, scene_type, task, batch_size, run_mono, skip_negative_elev=False, mono_sanity_check=False,  clean_percentage=0.0, mixture_percentages={'voice_and_location':.50, 'voice_only':.50}, **kwargs):
-        """
-        Builds a pytorch hdf5 dataset for v05 of binaural attention dataset
-        This class handles batching, returning batches of cues and scenes.
-        v05 uses pre-mixed scenes "signal" and "signal_co_located" for the mixed scene condition.
-        All cues are "voice_cue_target_loc" for v05: the target voice at the target location.
-        The "voice only" condition is achieved by using scenes where all signals are co-located. 
-
-        Parameters:
-            path (str): location of the hdf5 dataset
-            scene_type (str): string indicating whether to use both co-located and normal scenes, or just normal scenes.
-            task (str): string indicating label keys to return - word, location, or both
-            batch_size (int): batch size
-            run_mono (bool): whether to return mono signals or stereo signals (default)
-            skip_negative_elev (bool): whether to skip negative elevation labels (default False)
-            mono_sanity_check (bool): whether to use only left channel for both channels (default False)
-            mixture_percentages (dict): dictionary of percentages for each scene type in the mixed scene condition.
-                The keys are 'voice_and_location' and 'voice_only'. The values are floats that sum to 1.0.
-        Returns:
-            cue, scene, None, label - None returned instead of background for compatability with past versions. 
-        """
-        self.file_path = Path(path).as_posix()
-        self.dataset = None
-        self.task = task
-        self.run_mono = run_mono
-        self.batch_size = batch_size
-        self.skip_negative_elev = skip_negative_elev
-        self.mono_sanity_check = mono_sanity_check
-        self.batch_ixs = np.arange(self.batch_size)
-
-        self.cue_key = 'voice_cue_target_loc'
-        self.scene_type = scene_type
-        if scene_type == "mixed":
-            self.voice_and_loc_size = int(batch_size * mixture_percentages['voice_and_location'])
-            self.voice_only_percent_size = int(batch_size * mixture_percentages['voice_only'])
-
-        if self.task == 'word':
-            self.label_key = 'word_int'
-        elif self.task == 'location':
-            self.label_key = ('label_loc_target_azim', 'label_loc_target_elev')
-        elif self.task == 'word_and_location':
-            self.label_key = ('word_int', 'label_loc_target_azim', 'label_loc_target_elev')
-
-        with h5py.File(self.file_path, 'r', swmr=True) as file:
-            self.dataset_len = len(file['target']) // self.batch_size
-
-    def azim_elev_to_label(self, azim, elev):
-        if self.skip_negative_elev:
-            return np.array(((elev / 10) * 72) + (azim / 5) + 1, dtype=np.int64)
-        else:
-            return np.array((((elev + 30) / 10) * 72) + (azim / 5), dtype=np.int64)
-
-    def __getitem__(self, index):
-        """
-        Returns examples from the hdf5 file.
-        Args: 
-            index (int): index into the hdf5 file
-        Returns:
-            cue (np.array): the cue audio 
-            scene (np.array): the scene audio 
-            None (None): None - instead of background for compatability with past versions
-            label (np.array): the label for the batch.
-        """
-        start = index * self.batch_size
-        end = start + self.batch_size
-        if self.dataset is None:
-            self.dataset = h5py.File(self.file_path, 'r', swmr=True)
-
-        cue = self.dataset['voice_cue_target_loc'][start:end].transpose((0, 2, 1))
-
-        if self.scene_type == 'mixed':
-            # get cut sizes for each condition
-            normal_ix = start + self.voice_and_loc_size
-            co_located_ix = normal_ix + self.voice_only_percent_size    
-            ## get backgrounds for mixed cue condition
-            # get backgrounds for location only and voice + location conditions
-            scene_1 = self.dataset['signal'][start : normal_ix].transpose((0, 2, 1))
-            scene_2 = self.dataset['signal_co_located'][normal_ix : co_located_ix].transpose((0, 2, 1))
-            # first 2/3 normal, last 1/3 co-located
-            scene = np.concatenate((scene_1, scene_2), axis=0) 
-        else:
-            scene = self.dataset['signal'][start:end].transpose((0, 2, 1))
-
-        if self.task == 'word':
-            label = self.dataset[self.label_key][start:end]
-        elif self.task == 'location':
-            azim = self.dataset[self.label_key[0]][start:end]
-            elev = self.dataset[self.label_key[1]][start:end]
-            label = []
-            for azim, elev in zip(azim, elev):
-                label.append(self.azim_elev_to_label(azim, elev))
-        else:
-            word = self.dataset[self.label_key[0]][start:end]
-            azim = self.dataset[self.label_key[1]][start:end]
-            elev = self.dataset[self.label_key[2]][start:end]
-            loc = self.azim_elev_to_label(azim, elev)
-            label = np.stack((word, loc), axis=1)
-
-        if self.run_mono:
-            # average l & r channels
-            cue = cue.mean(1).reshape(self.batch_size, -1)
-            scene = scene.mean(1).reshape(self.batch_size, -1)
-            
-        if self.mono_sanity_check:
-            # use only left channel for both channels
-            cue[:,1,:] = cue[:,0,:]
-            scene[:,1,:] = scene[:,0,:]
-            scene[:,1,:] = scene[:,0,:]
-            
-        return cue, scene, None, label
-
-    def __len__(self):
-        return self.dataset_len
-
 
 class H5DatasetV06(torch.utils.data.Dataset):
-    def __init__(self, path, scene_type, task, batch_size, run_mono, skip_negative_elev=False, mono_sanity_check=False,  clean_percentage=0.0, mixture_percentages={'voice_and_location':.50, 'voice_only':.50}, no_cue_on_clean=False, **kwargs):
+    def __init__(self, path, scene_type, task, batch_size, run_mono, skip_negative_elev=False, mono_sanity_check=False,  clean_percentage=0.0, mixture_percentages={'voice_and_location':.50, 'voice_only':.50}, no_cue_on_clean=False, return_azim_loc_only=False, **kwargs):
         """
         Builds a pytorch hdf5 dataset for v06 of binaural attention dataset
         This class handles batching, returning batches of cues and scenes.
@@ -417,7 +145,7 @@ class H5DatasetV06(torch.utils.data.Dataset):
         self.batch_ixs = np.arange(self.batch_size)
         self.ix_probs = torch.tensor([1/batch_size] * batch_size) # uniform ix likelihoods 
         self.n_to_sample = int(self.clean_percentage * self.batch_size)
-      
+        self.return_azim_loc_only = return_azim_loc_only
         self.cue_key = 'voice_cue_target_loc'
         self.scene_type = scene_type
         if scene_type == "mixed":
@@ -427,6 +155,7 @@ class H5DatasetV06(torch.utils.data.Dataset):
             self.label_key = 'word_int'
         elif self.task == 'location':
             self.label_key = ['label_loc_target_azim', 'label_loc_target_elev']
+
         elif self.task == 'word_and_location':
             self.label_key = ('word_int', 'label_loc_target_azim', 'label_loc_target_elev')
 
@@ -439,6 +168,11 @@ class H5DatasetV06(torch.utils.data.Dataset):
         else:
             # + 40 is so lowest elevation is 0 in label index
             return np.array((((elev + 40) / 10) * 72) + (azim / 5), dtype=np.int64)
+        
+    def azim_to_label(self, azim):
+        if azim == 360:
+            azim = 0 # wrap around
+        return np.array((azim / 5), dtype=np.int64)
 
     def label_to_azim_elev(self, label):
         """
@@ -484,13 +218,19 @@ class H5DatasetV06(torch.utils.data.Dataset):
         elif self.task == 'location':
             azim = self.dataset[self.label_key[0]][start:end]
             elev = self.dataset[self.label_key[1]][start:end]
-            label = self.azim_elev_to_label(azim, elev)
-
+            if self.return_azim_loc_only:
+                label = self.azim_to_label(azim)
+            else:
+                label = self.azim_elev_to_label(azim, elev)
         else:
             word = self.dataset[self.label_key[0]][start:end]
             azim = self.dataset[self.label_key[1]][start:end]
             elev = self.dataset[self.label_key[2]][start:end]
-            loc = self.azim_elev_to_label(azim, elev)
+            if self.return_azim_loc_only:
+                loc = self.azim_to_label(azim)
+            else:
+                loc = self.azim_elev_to_label(azim, elev)
+
             label = np.stack((word, loc), axis=1)
 
         if self.clean_percentage > 0.0:
