b70b431cba20f4d44858f8e5a73e26615a093a8d
Ian <iangriffith@g.harvard.edu>
2025-10-08 03:01:26 -0400

    run anova

--- run_unit_tuning_anova.sh
+++ run_unit_tuning_anova.sh
@@ -5,31 +5,16 @@
 #SBATCH --mem=12Gb                           
 #SBATCH --cpus-per-task=12
 #SBATCH --time=4:00:00
-#SBATCH --partition=mit_preemptable
+#SBATCH --partition=use-everything
 #SBATCH --array=0-6 # 0-6 for full
-module add miniforge
 
+source /etc/profile.d/modules.sh
+module add openmind/miniconda
 export HDF5_USE_FILE_LOCKING=FALSE
 
-mamba activate pytorch_2
-
+source activate /om2/user/imgriff/conda_envs/pytorch_2_sva
 
 python3 src/unit_tuning_anova.py --model_name "word_task_v10_main_feature_gain_config_latest_ckpt" \
-                --analysis_dir "binaural_unit_activation_analysis" \
+                --analysis_dir "/om/scratch/Thu/imgriff/binaural_unit_activations_for_anova/" \
                 --layer_ix $SLURM_ARRAY_TASK_ID \
                 --n_jobs 12 \
-
-# python3 src/unit_tuning_anova.py --model_name "word_task_v09_cue_loc_task" \
-#                 --analysis_dir "binaural_unit_activations" \
-#                 --layer_ix $SLURM_ARRAY_TASK_ID \
-#                 --n_jobs 12 \
-
-# python3 src/unit_tuning_anova.py --model_name "word_task_v09_control_no_attn" \
-#                 --analysis_dir "binaural_unit_activations" \
-#                 --layer_ix $SLURM_ARRAY_TASK_ID \
-#                 --n_jobs 12 \
-
-# python3 src/unit_tuning_anova.py --model_name "word_task_conventional_layer_order" \
-#                 --analysis_dir "binaural_unit_activations" \
-#                 --layer_ix $SLURM_ARRAY_TASK_ID \
-#                 --n_jobs 12 \
